8ced9eff2d5a9d338f00deb6a11f4b1b88f506a1
Aryo <aryonp@gmail.com>
2018-01-02 10:20:37 +0700

    simplification

--- install.sh
+++ install.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
-
 apt-get install python-pip
-
 pip install speedtest-cli
-
 mkdir -p /var/SETT/
-
 cp sett.py /var/SETT/
-
 crontab -l > settcron
-
 echo "30 10,15 * * * python /var/SETT/sett.py" >> settcron
-
 crontab settcron
-
 rm settcron
